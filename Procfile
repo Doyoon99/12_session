507eed5a027937f154df786d219f1f2d7ec18495
이름 <>
2021-08-08 04:12:41 +0900

    for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vacation_4stproject.wsgi --log-file -
--- vacation_4stproject/settings.py
+++ vacation_4stproject/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qzs8%vvaj34e16rxd%3wy%r&62oqmt7tgzutykb$nc^3pelh9+'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'qzs8%vvaj34e16rxd%3wy%r&62oqmt7tgzutykb$nc^3pelh9+'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #모두 접근 가능하게 함 
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #반드시 이 자리에 추가
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#미디어 파일을 업로드하기 위함
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+
+#AWS
+
+AWS_ACCESS_KEY_ID = os.environ.get('AKIAU6PHFFK7LP5P6YNP')
+AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+AWS_STORAGE_BUCKET_NAME = 'doyoonbucket'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
